864ac310002a46e95c0fd338f0c4212d086cf8a3
Ewerson <vasconcelosdeveloper@outlook.com>
2018-12-28 22:28:05 -0200

    Estudo de classes com uso de sobre cargas e proprets

---
+++ ContaBancaria/Conta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaBancaria
+{
+    class Conta
+    {
+ 
+        public string Nome { get; set; }
+        public uint NumConta { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Conta(string nome, uint numConta, double depositoInicial)
+        {
+            Nome = nome;
+            NumConta = numConta;
+            Deposito(depositoInicial);
+            
+        }
+        public Conta(string nome, uint numConta)
+        {
+            Nome = nome;
+            NumConta = numConta;
+        }
+        public void Deposito(double deposito)
+        {
+            if(deposito > 0)
+            {
+                Saldo += deposito;
+            } else
+            {
+                Console.WriteLine(" Erro ao realizar o depósito, por favor tente novamente !");
+            }
+        } 
+        public void Saque(double saque)
+        {
+           Saldo -= saque + 5.00;
+        }
+        
+    }
+
+    
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositoInicial;
+            Conta novaConta;
+
+            Console.WriteLine("Cadastro de novo usuário: ");
+            Console.WriteLine("Por favor digite o nome do usuário");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Por favor digite o número da nova conta");
+            uint numConta = uint.Parse(Console.ReadLine());
+
+            Console.WriteLine("Deseja fazer um depósito inicial ? Sim(s) Não( Aperte qualquer tecla )");
+            string esc = Console.ReadLine();
+
+            if(esc == "s")
+            {
+                Console.WriteLine("Por favor digite o valor a ser depositado: ");
+                depositoInicial = double.Parse(Console.ReadLine());
+                novaConta = new Conta(nome, numConta, depositoInicial);
+            } else
+            {
+                novaConta = new Conta(nome, numConta);
+            }
+
+            Console.WriteLine("Deseja realizar um depósito: Sim (s) Não( Aperte qualquer tecla )");
+            string esc1 = Console.ReadLine();
+
+            if (esc1 == "s" || esc1 == "S")
+            {
+                Console.WriteLine("Por favor digite o valor a ser depositado: ");
+                double deposito = double.Parse(Console.ReadLine());
+                novaConta.Deposito(deposito);
+                Console.WriteLine("Deposito realizado com sucesso ! Saldo atual: " + novaConta.Saldo);
+            }
+
+            Console.WriteLine("Deseja realizar um saque: Sim (s) Não( Aperte qualquer tecla )");
+            string esc2 = Console.ReadLine();
+
+            if (esc2 == "s" || esc2 == "S")
+            {
+                Console.WriteLine("Por favor digite o valor do saque: ");
+                double saque = double.Parse(Console.ReadLine());
+                novaConta.Saque(saque);
+                Console.WriteLine("Saque realizado com sucesso ! Saldo atual: " + novaConta.Saldo);
+            }
+        }
+    }
+}
