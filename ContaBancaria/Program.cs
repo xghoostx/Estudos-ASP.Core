1fdeacbaf80b56e268c01393c155b1e3c9a403c8
Ewerson <vasconcelosdeveloper@outlook.com>
2018-12-28 23:13:32 -0200

    Update Program.cs
    Correção da condição if do deposito inicial

--- ContaBancaria/Program.cs
+++ ContaBancaria/Program.cs
@@ -18,7 +18,7 @@ namespace ContaBancaria
             Console.WriteLine("Deseja fazer um depósito inicial ? Sim(s) Não( Aperte qualquer tecla )");
             string esc = Console.ReadLine();
 
-            if(esc == "s" || esc1 == "S")
+            if(esc == "s" || esc == "S")
             {
                 Console.WriteLine("Por favor digite o valor a ser depositado: ");
                 depositoInicial = double.Parse(Console.ReadLine());
